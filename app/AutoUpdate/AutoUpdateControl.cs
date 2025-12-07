85ca8725a311d389b451b0ce0f35b60a20e00e97
Diego-JRH <djdompablo@gmail.com>
2025-12-07 17:40:51 +0100

    Version bump

--- app/AutoUpdate/AutoUpdateControl.cs
+++ app/AutoUpdate/AutoUpdateControl.cs
@@ -98,15 +98,7 @@ namespace GHelper.AutoUpdate
                     {
                         versionUrl = url;
                         update = true;
-
                         settings.SetVersionLabel(Properties.Strings.DownloadUpdate + $": {appVersion.Major}.{appVersion.Minor}.{appVersion.Build} → {tag}", true);
-
-                        string[] args = Environment.GetCommandLineArgs();
-                        if (force || args.Length > 1 && args[1] == "autoupdate")
-                        {
-                            AutoUpdate(url);
-                            return;
-                        }
 
                         string[] args = Environment.GetCommandLineArgs();
                         if (force || args.Length > 1 && args[1] == "autoupdate")
